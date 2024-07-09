cdb86d5627fd70e9e4f2aa2e3d15904304c2a980
jericho31 <62335652+jericho31@users.noreply.github.com>
2024-07-09 21:22:27 +0900

    Time: 152 ms (36%), Space: 33.9 MB (74.29%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.kt
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.kt
@@ -9,13 +9,20 @@
  */
 class Solution {
     fun swapPairs(head: ListNode?): ListNode? {
-        var cur = head
-        while (cur != null && cur.next != null) {
-            val temp = cur.`val`
-            cur.`val` = cur.next.`val`
-            cur.next.`val` = temp
-            cur = cur.next.next
+        if (head?.next == null) return head
+        val zero = ListNode(0)
+        zero.next = head
+
+        var cur = zero
+        while (cur.next?.next != null) {
+            val n1 = cur.next
+            val n2 = n1.next
+            val n3 = n2.next
+            cur.next = n2
+            n2.next = n1
+            n1.next = n3
+            cur = n1
         }
-        return head
+        return zero.next
     }
 }
