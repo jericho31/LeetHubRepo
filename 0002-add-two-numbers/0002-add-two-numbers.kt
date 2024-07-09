bc7a8bb865c34533054b7e25713c7b70bcc3d98d
jericho31 <62335652+jericho31@users.noreply.github.com>
2024-07-10 08:22:24 +0900

    Time: 255 ms (10.51%), Space: 45.9 MB (5.17%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.kt
+++ 0002-add-two-numbers/0002-add-two-numbers.kt
@@ -7,39 +7,42 @@
  *     var next: ListNode? = null
  * }
  */
-import java.math.BigInteger
-
 class Solution {
-    private fun ListNode?.toAllString(): String {
-        if (this == null) return ""
-        val sb = StringBuilder()
-        var cur = this
-        while (cur != null) {
-            sb.append(cur.`val`.toString())
-            cur = cur.next
-        }
-        return sb.toString()
-    }
-
-    private fun String.toListNode(): ListNode? {
-        if (this.any { !it.isDigit() }) return null
-        var cur = ListNode(0)
-        val zero = cur
-        for (c in this) {
-            cur.next = ListNode(c - '0')
-            cur = cur.next
-        }
-        return zero.next
-    }
-
-    private fun ListNode?.toReversedBigInteger(): BigInteger =
-        BigInteger(this.toAllString().reversed())
-    
-    private fun BigInteger.toReversedListNode(): ListNode {
-        return this.toString().reversed().toListNode()!!
-    }
-
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        return (l1.toReversedBigInteger() + l2.toReversedBigInteger()).toReversedListNode()
+        if (l1 == null) return l2
+        else if (l2 == null) return l1
+        val zero = ListNode(0)
+        var cur = zero
+        var n1 = l1
+        var n2 = l2
+        var carry = 0
+        while (n1 != null && n2 != null) {
+            val add = n1.`val` + n2.`val` + carry
+            if (add > 9) {
+                cur.next = ListNode(add - 10)
+                carry = 1
+            } else {
+                cur.next = ListNode(add)
+                carry = 0
+            }
+            cur = cur.next
+            n1 = n1.next
+            n2 = n2.next
+        }
+        var n3 = if (n1 == null) n2 else n1
+        while (n3 != null) {
+            val add = n3.`val` + carry
+            if (add != 10) {
+                cur.next = ListNode(add)
+                cur.next.next = n3.next
+                carry = 0
+                break
+            }
+            cur.next = ListNode(0)
+            cur = cur.next
+            n3 = n3.next
+        }
+        if (carry == 1) cur.next = ListNode(1)
+        return zero.next
     }
 }
