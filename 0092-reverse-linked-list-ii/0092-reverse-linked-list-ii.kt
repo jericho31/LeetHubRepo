45708cc6b3836abf559b9eb5bf03e25cb9955fbe
jericho31 <62335652+jericho31@users.noreply.github.com>
2024-07-10 00:04:40 +0900

    Time: 133 ms (86.77%), Space: 34.4 MB (8.99%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.kt
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.kt
@@ -8,42 +8,25 @@
  * }
  */
 class Solution {
-    private fun List<Int>.toListNode(): ListNode? {
-        if (this.isEmpty()) return null
-        var cur = ListNode(0)
-        val zero = cur
-        for (n in this) {
-            cur.next = ListNode(n)
-            cur = cur.next
-        }
-        return zero.next
-    }
-
-    private fun reverseList(head: ListNode?): ListNode? {
-        val list = mutableListOf<Int>()
-        var cur = head
-        while (cur != null) {
-            list += cur.`val`
-            cur = cur.next
-        }
-        return list.reversed().toListNode()
-    }
-
     fun reverseBetween(head: ListNode?, left: Int, right: Int): ListNode? {
         if (left >= right) return head
         val zero = ListNode(0)
         zero.next = head
 
         var before = zero
-        for (i in 1 until left) before = before!!.next
-        var last = before.next
-        for (i in left until right) last = last!!.next
-        var after = last!!.next
-
-        last.next = null
-        before!!.next = reverseList(before.next)
-        while (before!!.next != null) before = before.next
-        before.next = after
+        for (i in 1 until left) before = before.next
+        val betweenLast = before.next
+        
+        var prev = before.next
+        var cur = prev.next
+        for (i in left until right) {
+            val next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
+        }
+        before.next = prev
+        betweenLast.next = cur
         return zero.next
     }
 }
