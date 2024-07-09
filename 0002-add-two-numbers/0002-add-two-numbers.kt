26d10557aaa7dc98bf1c578cdb6854504a1ec5cf
jericho31 <62335652+jericho31@users.noreply.github.com>
2024-07-10 08:23:31 +0900

    Time: 204 ms (77.93%), Space: 39.2 MB (67.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.kt
+++ 0002-add-two-numbers/0002-add-two-numbers.kt
@@ -5,7 +5,7 @@
  * Definition for singly-linked list.
  * class ListNode(var `val`: Int) {
  *     var next: ListNode? = null
- * }
+ * } 
  */
 class Solution {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
